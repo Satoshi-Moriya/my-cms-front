f58f2e38c4efb3fdd6da8cedb6e4e321eed538f4
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2024-11-27 09:39:00 +0900

    es lintの設定を追加した（import文の順番、jsxのpropsの順番、tailwind、prettierとの衝突、vscodeの拡張機能） #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang='ja'>
       <body className={`antialiased`}>{children}</body>
     </html>
   );
