e135debd699fd4da6b29e7ca063ab37f27309da5
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2025-03-12 20:56:58 +0900

    prismaのwhereと動的な値（slug）を合わせないとでは更新できないっぽいが一旦commit  #42

--- src/app/schema.ts
+++ src/app/schema.ts
@@ -8,3 +8,13 @@ export const postSchema = z.object({
   status: z.enum(["publish", "draft"]),
   content: z.string().optional(),
 });
+
+export const updatePostSchema = z.object({
+  id: z.number(),
+  title: z
+    .string({ required_error: "タイトルは必須です" })
+    .max(100, "タイトルは100文字以内で入力してください"),
+  slug: z.string({ required_error: "スラッグは必須です" }),
+  status: z.enum(["publish", "draft"]),
+  content: z.string().optional(),
+});
